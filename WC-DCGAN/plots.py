5fce7321951b8e5b415bdf4767a2c2c17813b245
papz2000 <62347615+papz2000@users.noreply.github.com>
2024-07-05 18:44:59 +0530

    Update plots.py

--- WC-DCGAN/plots.py
+++ WC-DCGAN/plots.py
@@ -2,17 +2,17 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open('gen_loss.pkl', 'rb') as file:
+with open('/u/student/2022/cs22mtech14005/Single-Source-domain-generalized-zero-shot-object-detection/WC-DCGAN/loss_files/gen_loss.pkl', 'rb') as file:
     gen_loss = pickle.load(file)
-with open('class_loss.pkl', 'rb') as file:
+with open('/u/student/2022/cs22mtech14005/Single-Source-domain-generalized-zero-shot-object-detection/WC-DCGAN/loss_files/class_loss.pkl', 'rb') as file:
     class_loss = pickle.load(file)    
-with open('disc_loss.pkl', 'rb') as file:
+with open('/u/student/2022/cs22mtech14005/Single-Source-domain-generalized-zero-shot-object-detection/WC-DCGAN/loss_files/disc_loss.pkl', 'rb') as file:
     disc_loss = pickle.load(file)   
-with open('bus_loss.pkl', 'rb') as file:
+with open('/u/student/2022/cs22mtech14005/Single-Source-domain-generalized-zero-shot-object-detection/WC-DCGAN/loss_files/bus_loss.pkl', 'rb') as file:
     bus_loss = pickle.load(file) 
-with open('mode_loss.pkl', 'rb') as file:
+with open('/u/student/2022/cs22mtech14005/Single-Source-domain-generalized-zero-shot-object-detection/WC-DCGAN/loss_files/mode_loss.pkl', 'rb') as file:
     mode_loss = pickle.load(file) 
-with open('retrain_loss.pkl', 'rb') as file:
+with open('/u/student/2022/cs22mtech14005/Single-Source-domain-generalized-zero-shot-object-detection/WC-DCGAN/loss_files/retrain_loss.pkl', 'rb') as file:
     retrain_loss = pickle.load(file) 
 gen_combined = []
 for i in range(gen_loss.__len__()):
@@ -20,7 +20,7 @@ for i in range(gen_loss.__len__()):
 
 losses = {'gen_loss' : [gen_loss,'Generator loss'],'class_loss' : [class_loss,'Classification loss']
           ,'disc_loss' : [disc_loss,'Critic loss'],'bus_loss' : [bus_loss,'Unseen class classification loss']
-          ,'mode_loss' : [mode_loss,'Mode seeking loss'],'retrain_loss' : [retrain_loss,'Fine-tuning loss'],
+          ,'mode_loss' : [mode_loss,'Mode seeking loss'],'retrain_loss' : [retrain_loss,"Fine tuning loss"],
           'gen_combined' : [gen_combined,'Generator combined loss']}
 
 for loss in losses:
@@ -30,7 +30,7 @@ for loss in losses:
     modified_loss = [num / losses[loss][0].__len__() for num in losses[loss][0]]
     plt.plot(np.arange(0,modified_loss.__len__(),1), modified_loss, color="red")
     plt.show()
-    plt.savefig(loss)
+    plt.savefig(f"plots/{loss}")
     plt.close()
 
 
@@ -43,6 +43,5 @@ plt.xlabel('epoch')
 plt.ylabel('loss')
 plt.legend()
 plt.show()
-plt.savefig('overall WC-DCGAN Loss')
+plt.savefig('plots/overall WC-DCGAN Loss')
 plt.close()
-
